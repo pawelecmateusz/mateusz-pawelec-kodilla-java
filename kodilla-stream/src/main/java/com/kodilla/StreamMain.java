23d7fa379624d26c188f2a732bc9ea5de03052af
Mateusz Pawelec <mateuszpawelecprv@gmail.com>
2023-01-25 13:23:55 +0100

    7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -0,0 +1,39 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.sql.SQLOutput;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println("\n");
+        String text1 = "Just some poem";
+        String text2 = "Poem here, poem there";
+        poemBeautifier.beautify(text1, (text) -> "***" + text + "***");
+        poemBeautifier.beautify(text1, (text) -> text.toUpperCase());
+        poemBeautifier.beautify(text1, (text) -> text.replace("some", "a"));
+        poemBeautifier.beautify(text1, (text) -> "IT'S " + text.replace("some", "a").toUpperCase() + "!!!");
+        poemBeautifier.beautify(text2, (text) -> "<< " + text.replace("poem", "not") + " >>");
+        poemBeautifier.beautify(text2, (text) -> text + ", poem everywhere!");
+        System.out.println("\n");
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    }
+}
