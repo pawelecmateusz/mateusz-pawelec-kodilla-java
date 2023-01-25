10b9d70392854f0239d1d73d34570b534ef8ce22
Mateusz Pawelec <mateuszpawelecprv@gmail.com>
2023-01-25 21:05:15 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -9,6 +10,9 @@ import java.sql.SQLOutput;
 public class StreamMain {
 
     public static void main(String[] args) {
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         System.out.println("\n");
