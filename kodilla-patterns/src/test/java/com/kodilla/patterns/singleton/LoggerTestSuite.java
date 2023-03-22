9c64b39d07b244a4c67f116e2c6f52a2e2a0c8c9
Mateusz <122115887+pawelecmateusz@users.noreply.github.com>
2023-03-22 11:35:58 +0100

    15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @Test
+    void testGetLastLog() {
+        //Given
+        logger = Logger.INSTANCE;
+        //When
+        logger.log("LOGGER");
+        //Then
+        Assertions.assertEquals("LOGGER", logger.getLastLog());
+
+    }
+
+}
