a7d534d78b8f654497a3a26e764740fc4c954fe7
Mateusz <122115887+pawelecmateusz@users.noreply.github.com>
2023-04-10 11:18:05 +0200

    17.4

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -13,6 +15,9 @@ class CompanyDaoTestSuite {
 
     @Autowired
     private CompanyDao companyDao;
+
+    @Autowired
+    private EmployeeDao employeeDao;
 
     @Test
     void testSaveManyToMany() {
@@ -51,12 +56,74 @@ class CompanyDaoTestSuite {
         assertNotEquals(0, greyMatterId);
 
         //CleanUp
-        //try {
-        //    companyDao.deleteById(softwareMachineId);
-        //    companyDao.deleteById(dataMaestersId);
-        //    companyDao.deleteById(greyMatterId);
-        //} catch (Exception e) {
-        //    //do nothing
-        //}
+        try {
+            companyDao.deleteById(softwareMachineId);
+            companyDao.deleteById(dataMaestersId);
+            companyDao.deleteById(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+
+    @Test
+    void testEmployeesWithSpecificLastname() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee anneSmith = new Employee("Anne", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+        Employee owenGrady = new Employee("Owen", "Grady");
+
+        //When
+        employeeDao.save(johnSmith);
+        employeeDao.save(anneSmith);
+        employeeDao.save(stephanieClarckson);
+        employeeDao.save(lindaKovalsky);
+        employeeDao.save(owenGrady);
+
+        List<Employee> smithEmployees = employeeDao.retrieveLastname("Smith");
+        List<Employee> gradyEmployees = employeeDao.retrieveLastname("Grady");
+
+        //Then
+        try {
+            assertEquals(2, smithEmployees.size());
+            assertEquals(1, gradyEmployees.size());
+        } finally {
+            //CleanUp
+            employeeDao.deleteAll();
+        }
+    }
+
+    @Test
+    void testCompaniesNamesStartingWith() {
+        //Given
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company dataMasters = new Company("Data Masters");
+        Company dataNoobs = new Company("Data Noobs");
+        Company greyMatter = new Company("Grey Matter");
+        Company greyOrNot = new Company("Grey Or Not");
+
+        //When
+        companyDao.save(softwareMachine);
+        companyDao.save(dataMaesters);
+        companyDao.save(dataMasters);
+        companyDao.save(dataNoobs);
+        companyDao.save(greyMatter);
+        companyDao.save(greyOrNot);
+
+        List<Company> sofCompanies = companyDao.findCompaniesWithNameStartingWith("Sof");
+        List<Company> datCompanies = companyDao.findCompaniesWithNameStartingWith("Dat");
+        List<Company> greCompanies = companyDao.findCompaniesWithNameStartingWith("Gre");
+
+        //Then
+        try {
+            assertEquals(1, sofCompanies.size());
+            assertEquals(3, datCompanies.size());
+            assertEquals(2, greCompanies.size());
+        } finally {
+            //CleanUp
+            companyDao.deleteAll();
+        }
     }
 }
